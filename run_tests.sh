3e78522b910a7edf205901d0fe529fc9296e789b
Andrew Clemons <andrew.clemons@gmail.com>
2024-10-12 20:33:35 +0900

    Update arm64 tests.
    Build the correct image for testing by passing platform.

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,9 @@
 set -e
 set -o pipefail
 
-docker buildx build --load -t aclemons/slack-lambda:15.0 -f stable/Dockerfile --progress plain .
-docker buildx build --load -t aclemons/slack-lambda:current -f current/Dockerfile --progress plain .
-docker buildx build --load -t aclemons/slack-lambda:current-arm -f current/Dockerfile --progress plain .
+docker buildx build --load --platform linux/amd64 -t aclemons/slack-lambda:15.0 -f stable/Dockerfile --progress plain .
+docker buildx build --load --platform linux/amd64 -t aclemons/slack-lambda:current -f current/Dockerfile --progress plain .
+docker buildx build --load --platform linux/arm64/v8 -t aclemons/slack-lambda:current-arm -f current/Dockerfile --progress plain .
 
 docker compose up -d --build --wait --quiet-pull
 
